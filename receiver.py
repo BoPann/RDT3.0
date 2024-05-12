5c4d8cff29a93ac949265f4aa2b8d0ab89474c8d
bo <bo@BP>
2024-05-12 14:51:30 -0700

    Fix receiver by adding prev_seq vrb & removing while loop

--- receiver.py
+++ receiver.py
@@ -6,58 +6,46 @@ import util
 
 PORT = 10101
 SERVER_NAME = 'localhost'
-SENDER_SERVER = ('localhost', 10100)
 
-p_number = 1
+p_number = 0
+# machine state
+prev_seq = 0
+
+
+
+print('Receiver listening on port', PORT)
+# create socket
+s = socket(AF_INET, SOCK_DGRAM)
+s.bind((SERVER_NAME, PORT))
+
+# keep listening
 while True:
-    # create socket
-    with socket(AF_INET, SOCK_DGRAM) as s:
-        s.bind((SERVER_NAME, PORT))
 
-        print('Server listening on port', PORT)
-        # machine state
-        state = 0
+    # receive the packet
+    data, addr = s.recvfrom(2056)
+    print(f"Packet num.{p_number} received: {data}")
+    p_number += 1
+    
+    # make sleep to simulate timeout
+    # flag is used so only sleep once on the same packet
+    if p_number % 6 == 0:
+        # Simulate packet loss every 6 packets
+        print('simulating packet loss: sleep a while to trigger timeout event on the sender side...')
+        sleep(10)
 
-        # keep listening
-        while True:
-            # receive the packet
-            data, addr = s.recvfrom(2056)
-            print(f"Packet num.{p_number} received: {data}")
-            
-            # make sleep to simulate timeout
-            # flag is used so only sleep once on the same packet
-            timeout_flag = True
-            while p_number % 6 == 0 and timeout_flag:
-                sleep(3.5)
-                print("simulating packet loss: sleep a while to trigger timeout event on the sender side")
-                timeout_flag = not timeout_flag
-                
-
-            # check seq number
-            ack, seq = util.getAckSeq(data)
-            ack = int(ack)
-            seq = int(seq)
-            # when seq and state not match -> have sender resend
-            if seq != state:
-                print("Sequence number mismatch. Resending ACK for the previous sequence number.")
-                # Resend acknowledgment for the previous sequence number
-                packet = util.make_packet('', (ack + 1) % 2, (seq + 1) % 2)
-                s.sendto(packet, SENDER_SERVER)
-                break
-            
-            # check checksum
-            if not util.verify_checksum(data):
-            # checksum not match(corrupted) -> have sender resend
-                print("simulating packet bit errors/corrupted: ACK the previous packet!")
-                packet = util.make_packet('', (ack + 1) % 2, (seq + 1) % 2)
-                break
-            print(f"packet is expected, message string delivered: {util.getMsg(data)}")
-            # chcksum match, deliver data to application and send ack to sender
-            print(f"packet num. {p_number} is delivered, now creating and sending the ACK packet")
-            packet = util.make_packet('', ack, seq)
-            s.sendto(data, addr)
-            print("all done for this packet!\n\n")
-
-            # flip the state and increase packet number
-            state = (state + 1) % 2
-            p_number += 1
+    elif not util.verify_checksum(data) or p_number % 3 == 0:
+        print('simulating packet bit errors/corruption: ACK the previous packet!')
+        response_packet = util.make_packet('', 1, prev_seq)
+        s.sendto(response_packet, addr)
+    else:
+        print(f'packet is expected, message string delivered: {util.getMsg(data)}')
+        print('packet is delivered, now creating and sending the ACK packet...')
+        # Determine seq num from the received packet
+        seq = 1 if (data[11] & 1) == 1 else 0
+        # Create response packet with ACK for the current seq bum 
+        response_packet = util.make_packet('', 1, prev_seq)
+        s.sendto(response_packet, addr)
+        # Update previous seq num
+        previous_seq = seq
+        
+    print('all done for this packet!\n')
